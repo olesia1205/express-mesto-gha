26827249278a7323d879bda0c68434e637cb995b
Olesia Erastova <olesia1205@yandex.ru>
2023-03-29 21:23:27 +0300

    added controllers for like and dislike cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -7,7 +7,6 @@ module.exports.getCards = (req, res) => {
 };
 
 module.exports.createCard = (req, res) => {
-  // console.log(req.user._id);
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
@@ -16,8 +15,20 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.deleteCard = (req, res) => {
-  console.log(req.params.cardId);
+  // console.log(req.params.cardId);
   Card.findByIdAndDelete(req.params.cardId)
     .then((card) => res.status(201).send(card))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.putLike = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .then((user) => res.status(200).send(user))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.deleteLike = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .then((user) => res.status(200).send(user))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
-const { getCards, deleteCard, createCard } = require('../controllers/cards');
+const {
+  getCards, deleteCard, createCard, putLike, deleteLike,
+} = require('../controllers/cards');
 
 router.get('/', getCards);
 router.delete('/:cardId', deleteCard);
 router.post('/', express.json(), createCard);
+router.put('/:cardId/likes', putLike);
+router.delete('/:cardId/likes', deleteLike);
 
 module.exports = router;
