58e723d1500b8b63dfe5763ecdb74fab87631f1f
Olesia Erastova <olesia1205@yandex.ru>
2023-04-02 12:22:26 +0300

    добавила обработку несуществующего url

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const cardsRouter = require('./cards');
 
 routes.use('/users', usersRouter);
 routes.use('/cards', cardsRouter);
+routes.use('*', (req, res) => {
+  res.status(404).send({ message: 'По указанному url ничего нет' });
+});
 
 module.exports = routes;
