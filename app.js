2c0c60a89b950ae3c293b8ead41f587c48ce79ec
Olesia Erastova <olesia1205@yandex.ru>
2023-04-03 13:40:39 +0300

    вернула константу MONGOURL для прохождения тестов

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const routes = require('./routes');
 
 const app = express();
 
+const {
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
+} = process.env;
+
 app.use((req, res, next) => {
   req.user = {
     _id: '6424200f18e41c07a03eb9e8',
@@ -15,7 +19,7 @@ app.use((req, res, next) => {
 app.use('/', routes);
 
 async function connect() {
-  await mongoose.connect(process.env.MONGO_URL, {});
+  await mongoose.connect(MONGO_URL, {});
   // console.log(`Server connected db ${process.env.MONGO_URL}`);
   await app.listen(process.env.PORT);
   // console.log(`Server listen port ${process.env.PORT}`);
